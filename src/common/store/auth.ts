ad39f7386a2fa41e6b763ff3d153fa5a4aaca516
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-02-17 15:26:44 +0900

    Update auth.ts

--- src/common/store/auth.ts
+++ src/common/store/auth.ts
@@ -62,7 +62,7 @@ export const useAuthStore = defineStore("auth", {
     async reissue(origin: any) {
       try {
         const res = await axiosInstance.get("/api/auth/reissue");
-        console.log(res.headers.authorization)
+        console.log(res.headers)
         origin.headers.Authorization = res.headers.authorization;
         axiosInstance.defaults.headers.common.Authorization =
           res.headers.authorization;
