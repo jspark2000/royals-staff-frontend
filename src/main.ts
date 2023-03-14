4e7bb306e7d5a9155dc1bf01898d49b1abfadccf
jspark2000 <idpjs2000@gmail.com>
2023-03-14 15:14:59 +0900

    feat: add apexchart packages

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ import { createPinia } from "pinia";
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 
+import VueApexCharts from "vue3-apexcharts";
+
 import Vue3EasyDataTable from "vue3-easy-data-table";
 import "vue3-easy-data-table/dist/style.css";
 
@@ -25,6 +27,7 @@ const app = createApp(App);
 registerPlugins(app);
 app.component("EasyDataTable", Vue3EasyDataTable);
 app.use(VueSweetalert2);
+app.use(VueApexCharts);
 app.use(router);
 app.use(createPinia());
 app.mount("#app");
